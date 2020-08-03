d623c7e467cb2c57ee3eac009e52ec4a69b06929
reimon <reimonsk8@gmail.com>
2020-08-02 23:20:49 -0700

    first commit for cybperpunk style theme

--- client/src/components/ShopItem.js
+++ client/src/components/ShopItem.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './ShopItem.css'
 import {Image} from 'semantic-ui-react'
 
 const Shopitem = ({name, img}) =>{
     return(
-        <div>
+        <div id="item">
             <Image src={img} size='medium' centered ></Image>
             <p>{name}</p>
         </div>
